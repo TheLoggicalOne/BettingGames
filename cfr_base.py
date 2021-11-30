b4e7417bc68f5b92d0863428447a4d3c98f98b8c
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2021-11-30 05:01:24 +0330

    Just Starting and outlining required components for CFR

---
+++ cfr_base.py
@@ -0,0 +1,12 @@
+"""
+Basics components of counterfactual regret minimization algorithms: definition and implementation
+
+    Strategy
+    Reach Probabilities
+    Utilities
+    Counterfactual Utilities
+    Regret
+"""
+import numpy as np
+import betting_games as bg
+
