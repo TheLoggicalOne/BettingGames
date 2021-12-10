9a7df6289a0604d901db1c4350e8c91cd033bd47
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2021-12-10 20:56:13 +0330

    1)Continue the new approach of two strategy matrix instead of one Change all methods so that they work with new approach and previous changes to self.strategy_base  and self.uniform_strategy() Every thing seems to work fine and very good, but still I will test it 2)edit a mistake in self.uniform_strategy() method 3) add attribute self.turn 4) archive previous version in strategy_old.py

--- strategy.py
+++ strategy.py
@@ -1,4 +1,7 @@
 # TO BE COMPLETED
+# Start of effort to solve the "PROBLEM"
+# What is the problem?
+#
 """ Can Each Strat just be a list of probs?
  Strat as list of  willingness probs of coming to public node k. Note that each public node k  has only one parent
  which is accessible by G.public_state[k].parent
@@ -22,14 +25,13 @@ class Strategy:
      node i from its parent holding given hand by the player who is to act at the parent of i:
          players hand:[op_hand, ip_hand]
          i column of strategy matrix which relates to reach_player[i] = 1-to_move[i] Then
-         S[reach_player[i], hand[reach_player[i], i]
-         So game dynamic is as follows:
+         S[reach_player[i], hand[reach_player[i]], i]
+
+         For given strategy, we want to compute reach probabilities and state values and game dynamic....
          reaching node i from its parent: S[reach_player[i], : , i]
          reaching node i by following path pj for j=1,...,d:
          S[reach_player[i], : ,
     strategy_base: this is the main input
-
-
     """
     def __init__(self, game, strategy_base=None):
         self.game = game
@@ -49,62 +51,54 @@ class Strategy:
         self.op_turn_nodes = np.array([node for node in self.game.node if self.game.public_state[node].to_move == 0])
         self.ip_turn_nodes = np.array([node for node in self.game.node if self.game.public_state[node].to_move == 1])
         self.depth_of_node = self.game.depth_of_node
+        self.turn = [self.depth_of_node[i] % 2 for i in self.game.node]
         self.reach_player = [1 - (self.depth_of_node[i] % 2) for i in self.game.node]
         self.parent = self.game.parent
 
     # even cols are op cols
-    def check_branch_strategy(self):
+    def check_branch_strategy(self, position):
         """ return strategy columns of decision nodes in check branch of game.
         First column corresponds to op first column in strategy_base: 1
         even indexed columns are for op """
-        return np.hstack((self.strategy_base[:, 1:2], self.strategy_base[:, 4:self.check_decision_branch[-1] + 1:6]
-                          ))
+        return np.hstack((self.strategy_base[position, :, 1:2],
+                          self.strategy_base[position, :, 4:self.check_decision_branch[-1] + 1:6] ))
 
     # odd cols are op cols
-    def bet_branch_strategy(self):
+    def bet_branch_strategy(self, position):
         """ return strategy columns of decision nodes in bet branch of game.
         First column corresponds to ip first column: 2
         odd indexed columns are op  """
-        return np.hstack((self.strategy_base[:, 2:3], self.strategy_base[:, 7:self.bet_decision_branch[-1] + 1:6]))
+        return np.hstack((self.strategy_base[position, :, 2:3],
+                          self.strategy_base[position, :, 7:self.bet_decision_branch[-1] + 1:6]))
 
-    def reach_probs_of_check_decision_branch(self):
+    def reach_probs_of_check_decision_branch(self, position):
         """ return reach probability columns of decision nodes in check branch of game. First column corresponds to op
         first decision node  """
-        return np.cumprod(self.check_branch_strategy(), axis=1)
+        return np.cumprod(self.check_branch_strategy(position), axis=1)
 
-    def reach_probs_of_bet_decision_branch(self):
-        return np.cumprod(self.bet_branch_strategy(), axis=1)
+    def reach_probs_of_bet_decision_branch(self, position):
+        return np.cumprod(self.bet_branch_strategy(position), axis=1)
 
-    def op_reach_probs_of_check_decision_branch(self):
-        return np.cumprod(self.check_branch_strategy()[:, 0::2], axis=1)
-
-    def ip_reach_probs_of_check_decision_branch(self):
-        return np.cumprod(self.check_branch_strategy()[:, 1::2], axis=1)
-
-    def op_reach_probs_of_bet_decision_branch(self):
-        return np.cumprod(self.bet_branch_strategy()[:, 0::2], axis=1)
-
-    def ip_reach_probs_of_bet_decision_branch(self):
-        return np.cumprod(self.bet_branch_strategy()[:, 1::2], axis=1)
-
-    def reach_probs_calc(self, node):
+    def reach_probs_calc(self, node, position=None):
+        if position is None:
+            position = self.reach_player[node]
         if self.game.public_state[node].first_played_action == 'Check':
             if self.is_decision_node[node]:
-                return self.reach_probs_of_check_decision_branch()[:,
+                return self.reach_probs_of_check_decision_branch(position)[:,
                        self.depth_of_node[node] - 1:self.depth_of_node[node]]
             else:
                 parent = self.parent[node]
-                return self.strategy_base[:, node:node + 1] * self.reach_probs_of_check_decision_branch()[:,
-                                                              self.depth_of_node[parent] - 1:self.depth_of_node[parent]]
+                return self.strategy_base[position, :, node:node + 1] * self.reach_probs_of_check_decision_branch(
+                    position)[:, self.depth_of_node[parent] - 1:self.depth_of_node[parent]]
 
         else:
             if self.is_decision_node[node]:
-                return self.reach_probs_of_bet_decision_branch()[:,
+                return self.reach_probs_of_bet_decision_branch(position)[:,
                        self.depth_of_node[node] - 1:self.depth_of_node[node]]
             else:
                 parent = self.parent[node]
-                return self.strategy_base[:, node:node + 1] \
-                       * self.reach_probs_of_bet_decision_branch()[:,
+                return self.strategy_base[position, :, node:node + 1] \
+                       * self.reach_probs_of_bet_decision_branch(position)[:,
                          self.depth_of_node[parent] - 1:self.depth_of_node[parent]]
 
 # ----------------------------------- STRATEGY AND VALUES INDUCED BY STRATEGY ---------------------------------------- #
@@ -114,7 +108,7 @@ class Strategy:
         for _decision_node in self.decision_node:
             childs = self.game.public_state[_decision_node].children
             for child in childs:
-                S[self.reach_player[_decision_node], :, child:child+1] = np.full((
+                S[self.turn[_decision_node], :, child:child+1] = np.full((
                     self.number_of_hands, 1), 1/len(childs))
         return S
 
